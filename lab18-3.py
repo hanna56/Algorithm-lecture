2f19f731eadcfd739f482e7bce4ca1a9da087db0
Hanna <58249219+hanna56@users.noreply.github.com>
2021-07-21 15:32:06 +0900

    파트18 실습 과제3

---
+++ lab18-3.py
@@ -0,0 +1,51 @@
+# 이진트리의 후위순회 연산 구현
+class Node:
+
+    def __init__(self, item):
+        self.data = item
+        self.left = None
+        self.right = None
+
+
+    def inorder(self):
+        traversal = []
+        if self.left:
+            traversal += self.left.inorder()
+        traversal.append(self.data)
+        if self.right:
+            traversal += self.right.inorder()
+        return traversal
+
+
+    def postorder(self):
+        traversal = []
+        if self.left:
+            traversal += self.left.postorder()
+        if self.right:
+            traversal += self.right.postorder()
+        traversal.append(self.data)
+        return traversal
+
+
+class BinaryTree:
+
+    def __init__(self, r):
+        self.root = r
+
+
+    def inorder(self):
+        if self.root:
+            return self.root.inorder()
+        else:
+            return []
+
+
+    def postorder(self):
+        if self.root:
+            return self.root.postorder()
+        else:
+            return []
+
+
+def solution(x):
+    return 0
