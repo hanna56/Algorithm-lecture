402475dc2f5d6806123d145a9501024766563204
Hanna <58249219+hanna56@users.noreply.github.com>
2021-07-21 15:23:36 +0900

    파트 18 실습 과제1

---
+++ lab18-1.py
@@ -0,0 +1,43 @@
+# 이진트리의 depth() 연산 구현
+
+class Node:
+
+    def __init__(self, item):
+        self.data = item
+        self.left = None
+        self.right = None
+
+
+    def size(self):
+        l = self.left.size() if self.left else 0
+        r = self.right.size() if self.right else 0
+        return l + r + 1
+
+
+    def depth(self):
+        l = self.left.depth() if self.left else 0
+        r = self.right.depth() if self.right else 0
+        return max(l, r) + 1
+
+
+class BinaryTree:
+
+    def __init__(self, r):
+        self.root = r
+
+    def size(self):
+        if self.root:
+            return self.root.size()
+        else:
+            return 0
+
+
+    def depth(self):
+        if self.root:
+            return self.root.depth()
+        else:
+            return 0
+
+
+def solution(x):
+    return 0
